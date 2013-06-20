414c75f2c5e1164407f28c80f4a76cbc097760ae
Travis Ralston <travpc@gmail.com>
2013-06-19 23:32:14 -0600

    Phase 1 of no-plugin: Everythign but MCStats impl

--- src/main/java/com/github/rmsy/channels/listener/PlayerListener.java
+++ src/main/java/com/github/rmsy/channels/listener/PlayerListener.java
@@ -1,48 +1,45 @@
 package com.github.rmsy.channels.listener;
 
-import com.github.rmsy.channels.Channel;
-import com.github.rmsy.channels.ChannelsPlugin;
-import com.github.rmsy.channels.PlayerManager;
-import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import javax.annotation.Nonnull;
+import com.github.rmsy.channels.Channel;
+import com.github.rmsy.channels.ChannelsAPI;
+import com.github.rmsy.channels.PlayerManager;
+import com.google.common.base.Preconditions;
 
 /**
  * Listener for player-related events.
  */
-public class PlayerListener implements Listener {
+public class PlayerListener implements Listener{
 
-    /**
-     * The plugin.
-     */
-    private final ChannelsPlugin plugin;
+	/**
+	 * The plugin.
+	 */
+	private final ChannelsAPI plugin;
 
-    private PlayerListener() {
-        this.plugin = null;
-    }
+	/**
+	 * Creates a new PlayerListener.
+	 * 
+	 * @param plugin The API instance.
+	 */
+	public PlayerListener(@Nonnull final ChannelsAPI plugin){
+		this.plugin = Preconditions.checkNotNull(plugin, "plugin");
+	}
 
-    /**
-     * Creates a new PlayerListener.
-     *
-     * @param plugin The plugin.
-     */
-    public PlayerListener(@Nonnull final ChannelsPlugin plugin) {
-        this.plugin = Preconditions.checkNotNull(plugin, "plugin");
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerJoin(@Nonnull final PlayerJoinEvent event) {
-        Player player = Preconditions.checkNotNull(event, "event").getPlayer();
-        PlayerManager manager = this.plugin.getPlayerManager();
-        if (manager.getMembershipChannel(player) == null) {
-            Channel globalChannel = this.plugin.getGlobalChannel();
-            manager.setMembershipChannel(player, globalChannel);
-        }
-        player.addAttachment(this.plugin, ChannelsPlugin.GLOBAL_CHANNEL_PERMISSION, true);
-    }
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayerJoin(@Nonnull final PlayerJoinEvent event){
+		Player player = Preconditions.checkNotNull(event, "event").getPlayer();
+		PlayerManager manager = this.plugin.getPlayerManager();
+		if(manager.getMembershipChannel(player) == null){
+			Channel globalChannel = this.plugin.getGlobalChannel();
+			manager.setMembershipChannel(player, globalChannel);
+		}
+		player.addAttachment(this.plugin.getHost(), ChannelsAPI.GLOBAL_CHANNEL_PERMISSION, true);
+	}
 }
