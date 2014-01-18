3f8186b0e0887ed8d65ef1c4fcfb518f6cd3c5ef
Taylor Blau <me@ttaylorr.com>
2014-01-17 20:13:44 -0500

    Update pom.xml for repo transfer

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Clone sk89q-command-framework
-git clone https://github.com/rmsy/sk89q-command-framework
-cd sk89q-command-framework
-git checkout depend-fix
-mvn install -DskipTests=true
